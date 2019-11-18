9fc2aa02fd44dd8e5fa6b762179a2fd01d7d6066
MuizZer07 <muizxzer07@gmail.com>
2019-11-19 02:00:12 +0600

    firebase authentication with view

--- app/src/main/java/com/muizzer07/thunderstormmessenger/LoginActivity.kt
+++ app/src/main/java/com/muizzer07/thunderstormmessenger/LoginActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity: AppCompatActivity(){
@@ -12,6 +15,18 @@ class LoginActivity: AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
+        loginBtn.setOnClickListener{
+            val email = emailText.text.toString()
+            val password = passwordText.text.toString()
+            Log.d("LoginActivity", "EMAIL IS::: " + email)
+            if(email.isEmpty() || password.isEmpty() ) {
+                Toast.makeText(this, "Please enter email address and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            perform_login(email, password)
+        }
+
         registerText.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
@@ -19,4 +34,20 @@ class LoginActivity: AppCompatActivity(){
         };
     }
 
+    fun perform_login(email: String, password:String){
+        val auth = FirebaseAuth.getInstance()
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful){
+                        Log.d("LoginActivity", "Login Failed!")
+                        return@addOnCompleteListener
+                    }
+
+                    Toast.makeText(this, "Successfully Signed In!", Toast.LENGTH_SHORT).show()
+                    Log.d("LoginActivity", "Successfully Signed In!")
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+                }
+    }
 }
--- app/src/main/java/com/muizzer07/thunderstormmessenger/MainActivity.kt
+++ app/src/main/java/com/muizzer07/thunderstormmessenger/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -16,8 +18,12 @@ class MainActivity : AppCompatActivity() {
             val email = emailText.text.toString()
             val password = passwordText.text.toString()
 
-            Log.d("MainActivity", "Email is "+ email);
-            Log.d("MainActivity", "Password is "+ password);
+            if(email.isEmpty() || password.isEmpty() ) {
+                Toast.makeText(this, "Please enter email address and password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            perform_registration(email, password)
         }
 
         already_have_an_account_text.setOnClickListener {
@@ -26,5 +32,22 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    fun perform_registration(email: String, password:String){
+        val auth =  FirebaseAuth.getInstance()
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener {
+                    if(!it.isSuccessful){
+                        Log.d("MainActivity", "Registration Failed!")
+                        return@addOnCompleteListener
+                    }
+
+                    Toast.makeText(this, "Successfully Signed Up!", Toast.LENGTH_SHORT).show()
+                    Log.d("MainActivity", "Successfully created user!")
+                }
+                .addOnFailureListener{
+                    Toast.makeText(this, "${it.message}", Toast.LENGTH_SHORT).show()
+                }
     }
 }
